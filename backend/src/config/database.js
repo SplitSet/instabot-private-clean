0f549347088f9b9727b7257c8e5ecd34e744b271
Ankur Pandey <ankurpandey@Ankurs-MacBook-Air.local>
2025-09-17 00:00:58 +0530

    Restore User model and disable autoIndex to prevent duplicate warnings

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -12,7 +12,8 @@ const connectDB = async () => {
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
       family: 4, // Use IPv4, skip trying IPv6
-      autoIndex: process.env.NODE_ENV !== 'production', // Don't build indexes in production
+      autoIndex: false, // Disable automatic index building to prevent duplicate warnings
+      autoCreate: false, // Don't auto-create collections
       retryWrites: true,
     };
 
